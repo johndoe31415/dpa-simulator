2d9cd7f9c075e67ca09967960ce16719636abd3e
Johannes Bauer <joe@johannes-bauer.com>
2022-02-01 22:50:16 +0100

    Initial draft of embdedded code
    Compiles, but unfortunately does not produce the expected result.

---
+++ aes128_rom/aes128_rom.c
@@ -0,0 +1,70 @@
+/*
+	libthumb2sim - Emulator for the Thumb-2 ISA (Cortex-M)
+	Copyright (C) 2019-2019 Johannes Bauer
+
+	This file is part of libthumb2sim.
+
+	libthumb2sim is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; this program is ONLY licensed under
+	version 3 of the License, later versions are explicitly excluded.
+
+	libthumb2sim is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libthumb2sim; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+	Johannes Bauer <JohannesBauer@gmx.de>
+*/
+
+#include <stdbool.h>
+#include <thumb2sim/thumb2simguest.h>
+#include "aes128.h"
+
+int main(void) {
+	uint8_t key[16] = { 0 };
+	uint8_t plaintext[16] = { 0 };
+	uint8_t ciphertext[16];
+
+	__asm__ __volatile__("bkpt #1");
+	struct aes128_ctx_t aes;
+	aes128_init(&aes, key);
+	aes128_encrypt_block(&aes, plaintext, ciphertext);
+	__asm__ __volatile__("bkpt #2");
+
+	thumb2sim_write(&key, 16);
+	thumb2sim_write(&plaintext, 16);
+	thumb2sim_write(&ciphertext, 16);
+
+#if 0
+	/* Example of how to print strings in the emulator, for easy debugging */
+	thumb2sim_puts("Hello from the Cortex-M");
+
+	/* Example of how to write data to the emulator */
+	for (uint32_t i = 0; i < 12345; i++) {
+		if ((i & 255) == 0) {
+			thumb2sim_write(&i, 4);
+		}
+	}
+
+	/* Now we read integer values from the emulator, add them together with
+	 * 12345 and send the 32-bit result back */
+	struct {
+		uint32_t value_a;
+		uint32_t value_b;
+	} integer_read;
+	thumb2sim_read(&integer_read, sizeof(integer_read));
+	uint32_t result = integer_read.value_a + integer_read.value_b + 12345;
+	thumb2sim_write(&result, 4);
+
+	thumb2sim_puts("Goodbyte from the Cortex-M");
+	__asm__ __volatile__("bkpt #2");
+#endif
+
+	thumb2sim_exit(0);
+	return 0;
+}
---
+++ aes128_rom/emu_syscall.s
@@ -0,0 +1,35 @@
+/**
+	libthumb2sim - Emulator for the Thumb-2 ISA (Cortex-M)
+	Copyright (C) 2014-2019 Johannes Bauer
+
+	This file is part of libthumb2sim.
+
+	libthumb2sim is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; this program is ONLY licensed under
+	version 3 of the License, later versions are explicitly excluded.
+
+	libthumb2sim is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libthumb2sim; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+	Johannes Bauer <JohannesBauer@gmx.de>
+**/
+
+.syntax unified
+.cpu cortex-m4
+.fpu softvfp
+.thumb
+
+.section .text
+.type thumb2sim_syscall, %function
+thumb2sim_syscall:
+	bkpt #255
+	bx lr
+.size thumb2sim_syscall, .-thumb2sim_syscall
+.global thumb2sim_syscall
